78ada6c364b55fb5f4f8caac3ef51357d20f2b3d
caio <Caio.Souza@mkcorp.com>
2021-01-20 14:20:53 -0300

    Commit de projeto inicial

---
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -0,0 +1,75 @@
+package com.algaworks.osworks.api.controller;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import com.algaworks.osworks.domain.model.Cliente;
+import com.algaworks.osworks.domain.repository.ClienteRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@GetMapping
+	public List<Cliente> listar () {
+		return clienteRepository.findAll();
+	}
+	
+	@GetMapping("/{clienteId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+		Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+		
+		if (cliente.isPresent()) {
+			return ResponseEntity.ok(cliente.get());
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
+		
+		@PostMapping
+		@ResponseStatus(HttpStatus.CREATED)
+		public Cliente adicionar(@RequestBody Cliente cliente) {
+			return clienteRepository.save(cliente);
+		}
+		
+		
+		@PutMapping("/{clienteId}")
+		@GetMapping("/{clienteId}")
+		public ResponseEntity<Cliente> gravar(@PathVariable Long clienteId,
+														@RequestBody Cliente cliente){
+					@GetMapping
+					return clienteRepository.findById(clienteId).map(gravar -> {
+						
+					gravar.setNome(cliente.getNome());
+					gravar.setEmail(cliente.getEmail());
+					gravar.setTelefone(cliente.getTelefone());
+					
+					Cliente updated = clienteRepository.save(cliente);
+					
+					return ResponseEntity.ok(cliente.get()).body(updated);
+		}).orElse(ResponseEntity.notFound().build());
+
+		}
+	
+}
+
+
